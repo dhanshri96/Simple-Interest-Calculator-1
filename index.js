fa68c4d9f4b62e6bdcad008dfa5968b7c504a15a
David Noh <dnoh@Davids-MacBook-Pro.local>
2019-02-22 03:38:57 -0500

    functionality finished

---
+++ index.js
@@ -0,0 +1,35 @@
+window.onload=function(){
+    
+    document.getElementById("calcButton").addEventListener("click", calculate);
+
+}
+
+function calculate(){
+    var principal = parseFloat(document.getElementById("principal").value,10);
+    var years = parseFloat(document.getElementById("years").value,10);
+    var rate = parseFloat(document.getElementById("rate").value,10);
+
+    if(isNaN(principal) || isNaN(years) || isNaN(rate)){
+        
+        var oldNode = document.getElementById("result").childNodes[0];
+        document.getElementById("result").replaceChild(document.createTextNode("Incorrect values"), oldNode);
+
+    }
+    else{
+
+        var result = (principal * years * rate) / (100);
+        result = result.toFixed(2);
+        
+        var oldNode = document.getElementById("result").childNodes[0];
+        document.getElementById("result").replaceChild(document.createTextNode("$ " + result.toString()), oldNode);
+
+    }
+
+    document.getElementById("result").disabled=false;
+
+}
+
+function isEmpty(obj){
+    if(obj.toString() == "") return true;
+    return false;
+}
